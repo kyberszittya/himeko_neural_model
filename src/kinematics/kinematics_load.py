b68c314cd9f712e6fa18ec999742b915b7525590
Hajdu Csaba <kyberszittya@protonmail.ch>
2024-08-31 01:37:58 +0200

    Text generation added to reconstruct descriptions

--- src/kinematics/kinematics_load.py
+++ src/kinematics/kinematics_load.py
@@ -1,3 +1,4 @@
+from himeko.transformations.text.generate_text import generate_text
 from processing.parse_description import ParseDescriptionEdge
 
 
@@ -12,6 +13,7 @@ def main():
     print([x.name for x in root["left_leg"].get_all_children(lambda x: True)])
     print([x.name for x in root["right_wing"].get_all_children(lambda x: True)])
     print([x.name for x in root["left_wing"].get_all_children(lambda x: True)])
+    print(generate_text(root))
 
 
 if __name__ == "__main__":
