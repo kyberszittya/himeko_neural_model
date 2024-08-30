570ea25d8b3d5cc8d3c00a872f50e3b137af1276
Hajdu Csaba <kyberszittya@protonmail.ch>
2024-08-30 20:24:27 +0200

    Copy whole tree yea

--- src/kinematics/kinematics_load.py
+++ src/kinematics/kinematics_load.py
@@ -8,6 +8,10 @@ def main():
     h = e.execute(path=p, library_path=library_path)
     root = h[-1]
     print(root.name)
+    print([x.name for x in root["right_leg"].get_all_children(lambda x: True)])
+    print([x.name for x in root["left_leg"].get_all_children(lambda x: True)])
+    print([x.name for x in root["right_wing"].get_all_children(lambda x: True)])
+    print([x.name for x in root["left_wing"].get_all_children(lambda x: True)])
 
 
 if __name__ == "__main__":
