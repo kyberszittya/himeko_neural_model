7f32c7cc55431b851e6300420ed6cb0690684e44
Hajdu Csaba <kyberszittya@protonmail.ch>
2024-08-30 02:13:56 +0200

    Copy reference will be done tomorrow

--- src/kinematics/kinematics_load.py
+++ src/kinematics/kinematics_load.py
@@ -4,8 +4,10 @@ from processing.parse_description import ParseDescriptionEdge
 def main():
     e = ParseDescriptionEdge("parse_edge", 0, 0, b'0', b'0', "label", None)
     p = "../../data/kinematics/chicken_kinematics.himeko"
-    h = e.execute(path=p)
-    print(h)
+    library_path = "../../data/kinematics/"
+    h = e.execute(path=p, library_path=library_path)
+    root = h[-1]
+    print(root.name)
 
 
 if __name__ == "__main__":
