8e733ffd19ed0273d6209204ca37b4b0d2f97c2c
Hajdu Csaba <kyberszittya@protonmail.ch>
2024-09-06 20:29:11 +0200

    Making dataflow graph working... better than last time

--- src/kinematics/kinematics_load.py
+++ src/kinematics/kinematics_load.py
@@ -1,11 +1,27 @@
+from himeko.hbcm.elements.edge import EnumRelationDirection
+from himeko.hbcm.factories.creation_elements import FactoryHypergraphElements
 from himeko.transformations.text.generate_text import generate_text
 from processing.parse_description import ParseDescriptionEdgeFromFile, ParseDescriptionEdge
 
 
 def main():
-    e = ParseDescriptionEdgeFromFile("parse_edge", 0, 0, b'0', b'0', "label", None)
+    engine = FactoryHypergraphElements.create_vertex_default("engine", 0)
+    n_i0 = FactoryHypergraphElements.create_vertex_default("dataset", 0, engine)
+    n_h0 = FactoryHypergraphElements.create_vertex_default("hidden", 0, engine)
+    n_o0 = FactoryHypergraphElements.create_vertex_default("output", 0, engine)
+    #e = ParseDescriptionEdgeFromFile("parse_edge", 0, 0, b'0', b'0', "label", None)
+    e_parse: ParseDescriptionEdgeFromFile = FactoryHypergraphElements.create_edge_constructor_default(
+        ParseDescriptionEdgeFromFile, "parse_edge", 0, engine)
+    e_parse += (n_i0, EnumRelationDirection.IN, 1.0)
+    e_parse += (n_h0, EnumRelationDirection.OUT, 1.0)
     p = "../../data/kinematics/chicken_kinematics.himeko"
     library_path = "../../data/kinematics/"
+    FactoryHypergraphElements.create_attribute_default("path", p, "string", 0, n_i0)
+    FactoryHypergraphElements.create_attribute_default("library_path", library_path, "string", 0, n_i0)
+    e_parse.execute()
+    print(n_h0["root"])
+
+    """    
     h = e.execute(path=p, library_path=library_path)
     root = h[-1]
     print(root.name)
@@ -21,6 +37,7 @@ def main():
     h_text = ParseDescriptionEdge(
         "parse_edge", 0, 0, b'0', b'0', "label", None)
     h_text.execute(text=generate_text(root), library_path=library_path)
+    """
 
 
 if __name__ == "__main__":
